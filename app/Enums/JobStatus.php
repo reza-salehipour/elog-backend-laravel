107105a33bb72335dd2efa00ce30ee39e58e7ac9
Reza Salehipour <rez.salehipour@gmail.com>
2024-04-28 15:15:58 +0100

    create job status enum file

---
+++ app/Enums/JobStatus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+class JobStatus
+{
+    const OPEN = 'open';
+    const IN_PROGRESS = 'in_progress';
+    const COMPLETED = 'completed';
+    const CANCELLED = 'cancelled';
+}
